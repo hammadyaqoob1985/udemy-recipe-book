f750e912bd7dd0ba53db92f79f1a12bba7eb4347
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-05-13 18:12:56 +0100

    add shopping list service to get and add ingredients

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
+import { Ingredient } from "../shared/ingredient.model";
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+
 export class ShoppingListService {
-    
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 3),
+      ];
+
+      getIngredients() {
+          return this.ingredients.slice();
+      }
+
+      addIngredient(ingredient : Ingredient) {
+        this.ingredients.push(ingredient);
+      }
 }
